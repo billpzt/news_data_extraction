34b83c73a54cd7e046d5ee261cc2d7da3483362c
Billpzt <billpiazzetta@gmail.com>
2024-06-02 22:40:36 -0300

    Changed local to False

--- tasks.py
+++ tasks.py
@@ -3,5 +3,5 @@ from NewsExtractor import NewsExtractor as extractor
 
 @task
 def minimal_task():
-    ex = extractor(search_phrase="Iran", months=2, news_category="World & Nation", local=True)
+    ex = extractor(search_phrase="Iran", months=2, news_category="World & Nation", local=False)
     ex.run()
